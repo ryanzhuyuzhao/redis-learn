db869a3bd76ccfbba6c0cb4408add104fd625b24
ryan <ryan.zhu@uaf.com.cn>
2021-06-29 16:07:01 +0800

    commit

---
+++ src/main/java/org/myself/redislearn/App.java
@@ -0,0 +1,13 @@
+package org.myself.redislearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
